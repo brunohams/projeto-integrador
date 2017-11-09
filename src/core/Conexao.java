4f77ec6b41c3a4836d1f5f7a27c8dcac4326b1b5
Bruno <mail@brunohams.com.br>
2017-11-09 19:23:48 -0200

    Alterações estruturas de pastas

---
+++ src/core/Conexao.java
@@ -0,0 +1,104 @@
+
+package core;
+
+
+import java.io.Serializable;
+import java.sql.*;
+
+/**
+ * Classe conexão
+ */
+public final class Conexao implements Serializable
+{
+    private static Conexao instance;
+
+    private static final String DRIVER_JDBC = "org.gjt.mm.mysql.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/senai";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    
+    private Connection conn;
+    private PreparedStatement preparedStatement;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    public static Conexao getInstance() 
+    {
+        if (instance == null) 
+        {
+            instance = new Conexao();
+        }
+        return instance;
+    }
+    
+    private Conexao ()
+    {
+        carregaDriver();
+    }
+    
+    private void carregaDriver() 
+    {
+        try 
+        {
+            System.out.println("Carregando Driver JDBC...");
+            Class.forName(DRIVER_JDBC);
+            System.out.println(
+            "Driver JDBC carregado com sucesso...");
+        }
+        catch (ClassNotFoundException e) 
+        {
+            System.out.println(
+            "Driver JDBC nao foi carregado...");
+        }
+        
+    }
+    
+    private void abreConexao()
+    {
+        try 
+        {
+            System.out.println("Conectando ao banco...");
+            conn = (Connection) DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Conexao efetuada...");
+        } catch (SQLException e) 
+        {
+            System.out.println("Falha na conexao...");
+        }
+    }
+    
+    private void fechaConexao() 
+    {
+        try 
+        {
+            if (statement != null) 
+            {
+                statement.close();
+            }
+            conn.close();
+            System.out.println("Conexao fechada...");
+        } catch (SQLException e) 
+        {
+            System.out.println("Problemas no fechamento da conexao...");
+        }
+    }
+    
+    public void query(String sql) 
+    {
+        
+        abreConexao();
+        
+        try 
+        {
+            statement = (Statement) conn.createStatement();
+            statement.executeUpdate(sql);
+        } 
+        catch (SQLException e) 
+        {
+            System.out.println("Problemas na insercao..."+e.getMessage());
+        }
+        
+        fechaConexao();
+        
+    }
+  
+}
--- src/test/TesteConexao.java
+++ src/test/TesteConexao.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package PI;
+package test;
+
+import controller.Registro;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
